adcc38f19e3879193ec28065f82805b0e743b377
katyaHerrera <katya_yan.herrera@hotmail.com>
2016-06-19 10:57:53 -0600

    Agrego de vista principal

--- app/src/main/java/sv/edu/ues/fia/rumboamifuturo/VistaPrincipal.java
+++ app/src/main/java/sv/edu/ues/fia/rumboamifuturo/VistaPrincipal.java
@@ -18,23 +18,28 @@ public class VistaPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_vista_principal);
         imageButtonRegistro=(ImageButton)findViewById(R.id.imageButton);
         imageButtonListaU=(ImageButton)findViewById(R.id.imageButton2);
+        imageButtonRegistro.setOnClickListener(onclick);
+        imageButtonListaU.setOnClickListener(onclick);
 
-        imageButtonRegistro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent1= new Intent(VistaPrincipal.this,registroActivity.class);
-                startActivity(intent1);
-            }
-        });
-
-        imageButtonListaU.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(VistaPrincipal.this,ItemListActivity.class);
-                startActivity(intent);
-            }
-        });
     }
+    View.OnClickListener onclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.imageButton:
+                    Intent intent1=new Intent(VistaPrincipal.this,registroActivity.class);
+                    startActivity(intent1);
+                    break;
+                case  R.id.imageButton2:
+                    Intent intent=new Intent(VistaPrincipal.this,ItemListActivity.class);
+                    startActivity(intent);
+                    break;
+
+
+            }
+
+
+        }
+    };
 
 }
