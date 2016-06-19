3195963ace085bc3396b73549a589b32007d32ec
katyaHerrera <katya_yan.herrera@hotmail.com>
2016-06-19 13:00:06 -0600

    audio a la vista principal

--- app/src/main/java/sv/edu/ues/fia/rumboamifuturo/VistaPrincipal.java
+++ app/src/main/java/sv/edu/ues/fia/rumboamifuturo/VistaPrincipal.java
@@ -2,6 +2,7 @@ package sv.edu.ues.fia.rumboamifuturo;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +10,21 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class VistaPrincipal extends AppCompatActivity {
+    MediaPlayer Media;
     ImageButton imageButtonRegistro;
     ImageButton imageButtonListaU;
+    ImageButton imageButtonAudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_principal);
+
+        Media=MediaPlayer.create(getApplicationContext(),R.raw.audio_pag_principal);
+        imageButtonAudio=(ImageButton)findViewById(R.id.imageButton3);
         imageButtonRegistro=(ImageButton)findViewById(R.id.imageButton);
         imageButtonListaU=(ImageButton)findViewById(R.id.imageButton2);
+        imageButtonAudio.setOnClickListener(onclick);
         imageButtonRegistro.setOnClickListener(onclick);
         imageButtonListaU.setOnClickListener(onclick);
 
@@ -34,6 +41,16 @@ public class VistaPrincipal extends AppCompatActivity {
                     Intent intent=new Intent(VistaPrincipal.this,ItemListActivity.class);
                     startActivity(intent);
                     break;
+                case R.id.imageButton3:
+                    if(Media.isPlaying()){
+                        Media.pause();
+                        imageButtonAudio.setImageResource(R.drawable.ic_action_name4);
+                       }else{
+                        Media.start();
+                        imageButtonAudio.setImageResource(R.drawable.ic_action_name5);
+
+
+                    }
 
 
             }
